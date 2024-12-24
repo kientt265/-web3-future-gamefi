aa8d4318b43947321bd4aee2352ac7d747b0ca10
kientt265 <kientran26503@gmail.com>
2024-12-25 01:10:46 +0700

    build-chart

--- front/src/components/PriceTracker.tsx
+++ front/src/components/PriceTracker.tsx
@@ -1,8 +1,15 @@
 // PriceTracker.tsx
 import React, { useEffect, useState } from 'react';
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from 'chart.js';
+
+// Đăng ký tất cả các thành phần cần thiết
+Chart.register(...registerables);
 
 const PriceTracker = () => {
-    const [price, setPrice] = useState(null);
+    
+    const [price, setPrice] = useState<number | null>(null);
+    const [priceData, setPriceData] = useState<number[]>([]);
 
     useEffect(() => {
         const ws = new WebSocket('wss://stream.binance.com:9443/ws/!ticker@arr');
@@ -13,9 +20,10 @@ const PriceTracker = () => {
 
         ws.onmessage = (event) => {
             const prices = JSON.parse(event.data);
-            prices.forEach((ticker: { s: string; c: React.SetStateAction<null>; }) => {
+            prices.forEach((ticker: { s: string; c: string; }) => {
                 if (ticker.s === 'BTCUSDT') {
-                    setPrice(ticker.c);
+                    setPrice(parseFloat(ticker.c));
+                    setPriceData(prevData => [...prevData, parseFloat(ticker.c)]);
                 }
             });
         };
@@ -34,10 +42,25 @@ const PriceTracker = () => {
         };
     }, []);
 
+    // Dữ liệu cho biểu đồ
+    const data = {
+        labels: priceData.map((_, index) => index + 1),
+        datasets: [
+            {
+                label: 'BTCUSDT Price',
+                data: priceData,
+                fill: false,
+                borderColor: 'rgba(75,192,192,1)',
+                tension: 0.1
+            }
+        ]
+    };
+
     return (
         <div>
             <h1>Price Tracker</h1>
             {price ? <p>Price of BTCUSDT: {price}</p> : <p>Loading...</p>}
+            <Line data={data} />
         </div>
     );
 };
